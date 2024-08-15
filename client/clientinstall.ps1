7294ebd1cb864e7e6e64b5fef90c3a049a5cc94a
Anuragk <networkhunting@gmail.com>
2024-08-15 21:39:51 +1000

    Update clientinstall.ps1

--- client/clientinstall.ps1
+++ client/clientinstall.ps1
@@ -67,7 +67,6 @@ try {
     Break;
 }
 
-
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 #Setting up the stage 2 script execution
